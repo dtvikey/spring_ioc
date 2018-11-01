e4e0ba9ee5e38898ec1cd839275e991a56c38115
dtvikey <dtvikey@qq.com>
2018-11-01 11:15:04 +0800

    v1.0

--- src/main/java/com/imooc/ioc/demo5/CollectionBean.java
+++ src/main/java/com/imooc/ioc/demo5/CollectionBean.java
@@ -1,5 +1,7 @@
 package com.imooc.ioc.demo5;
 
+import java.util.*;
+
 /**
  * @Author: dtvikey
  * @Date: 18-11-1 上午11:00
@@ -7,6 +9,64 @@ package com.imooc.ioc.demo5;
  */
 public class CollectionBean {
 
+    private String[] arrs;
 
+    private List<String> list;
 
+    private Set<String> set;
+
+    private Map<String,Integer> map;
+
+    private Properties properties;
+
+    public String[] getArrs() {
+        return arrs;
+    }
+
+    public void setArrs(String[] arrs) {
+        this.arrs = arrs;
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+
+    public Set<String> getSet() {
+        return set;
+    }
+
+    public void setSet(Set<String> set) {
+        this.set = set;
+    }
+
+    public Map<String, Integer> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, Integer> map) {
+        this.map = map;
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+
+    @Override
+    public String toString() {
+        return "CollectionBean{" +
+                "arrs=" + Arrays.toString(arrs) +
+                ", list=" + list +
+                ", set=" + set +
+                ", map=" + map +
+                ", properties=" + properties +
+                '}';
+    }
 }
